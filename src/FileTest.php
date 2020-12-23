15c6df0b5508e51dd508ab9adeb909a1b7ab6c09
mengdechang <2326945617@qq.com>
2020-12-24 00:09:57 +0800

    first commit

---
+++ src/FileTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/12/24 0024
+ * Time: 上午 0:09
+ */
+
+class FileTest
+{
+    public function add()
+    {
+        return true;
+    }
+}
